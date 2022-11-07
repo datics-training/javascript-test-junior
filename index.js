bca790c4300091b533327998ff14ae39a9553549
Datmaniac <117644838+datics-training@users.noreply.github.com>
2022-11-07 11:54:03 +0000

    Add instruction to use unit test function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function leapYear(year)
   return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
 
-// Please write all possible test cases for this function based on the conditions above
+// Please write all possible test cases for this function based on the conditions above. Use the Unit Tests button (button with tick mark) in the left sidebar. 
 
 module.exports = {
   leapYear
